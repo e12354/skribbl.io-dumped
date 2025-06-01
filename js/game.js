1d339b9efb473ba9331356cf6655c7bc4d5fc364
e12354 <e12354real@proton.me>
2025-06-01 16:00:45 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1589,7 +1589,7 @@
     }
     ;
     let k = 4
-      , $n = "https://raw.githubusercontent.com/e12354/skribbl.io-dumped/refs/heads/main/server-point";
+      , $n = "https://e12354.github.io/skribbl.io-dumped/server-point";
     G;
     var w = []
       , x = 0
